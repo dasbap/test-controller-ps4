f87b49bbf6f2c686aab9f72fb889f157dff758f9
dasbap <dasilvabaptiste1411@gmail.com>
2026-04-14 18:32:28 +0200

    suppression vigemInstaller

--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ class Program
 
     static async Task Main()
     {
-        ViGEmInstaller.Run();
 
         cts = new CancellationTokenSource();
         Console.WriteLine("Démarrage de l'émulation manette...");
